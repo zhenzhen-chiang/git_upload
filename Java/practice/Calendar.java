de31312697fe7023ec12b13cd93e453c6e7db3f7
Joyce_00550348 <joyce41221@gmail.com>
2024-08-14 15:02:57 +0800

    【JAVA】評量第四次push

--- Java/practice/Calendar.java
+++ Java/practice/Calendar.java
@@ -3,42 +3,56 @@ package com.cathaybk.practice.nt50348.b;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calendar {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("輸入介於1-12之間的整數:");
-		int month = scanner.nextInt();
-
-		LocalDate today = LocalDate.now();
-		LocalDate firstDateOfMonth = today.withMonth(month).with(TemporalAdjusters.firstDayOfMonth());
-		LocalDate lastDateOfMoonth = firstDateOfMonth.with(TemporalAdjusters.lastDayOfMonth());
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年M月");
-		System.out.print("      " + firstDateOfMonth.format(formatter)+"\n");
-		System.out.println("---------------------");
-		System.out.println("日  一  二  三 四  五  六");
-		System.out.println("=====================");
-		int dayOfWeekStarIndex = firstDateOfMonth.getDayOfWeek().getValue();//獲取月份的第一天是星期幾(要從index裡面get出value)
-		if (dayOfWeekStarIndex == 7) {
-			dayOfWeekStarIndex = 0;//將星期天的位置換到index0
-		}
-		for (int i = 0; i < dayOfWeekStarIndex; i++) {
-			System.out.print("   ");//根據星期幾換算出前面需要幾個空格
-		}
-		LocalDate currentDay = firstDateOfMonth;//初始化currnetDay為這個月第一天
-		while (!currentDay.isAfter(lastDateOfMoonth)) {
-			System.out.printf("%2d ", currentDay.getDayOfMonth());
-			if (currentDay.getDayOfWeek().getValue() ==6) {
-				System.out.println();
+	public static void main(String[] args) {// 上傳前在F一次
+		try (Scanner scanner = new Scanner(System.in);) {// try with resource
+			int month;
+			while (true) {
+				try {
+					System.out.println("輸入介於1-12之間的整數:");
+					month = scanner.nextInt();
+					// 建立一個判斷，輸入只能在1-12之間try catch
+					if (month >= 1 && month <= 12) {
+						break;
+					} else {
+						System.out.println("錯誤，輸入數值不在1-12之間");
+					}
+				} catch (InputMismatchException e) {
+					scanner.next();// 清除之前輸入的數值，重新輸入
+				}
 			}
-			currentDay = currentDay.plusDays(1);
-		}
-		if (currentDay.getDayOfWeek().getValue() != 1) {
+
+			LocalDate today = LocalDate.now();
+			LocalDate firstDateOfMonth = today.withMonth(month).with(TemporalAdjusters.firstDayOfMonth());
+			LocalDate lastDateOfMoonth = firstDateOfMonth.with(TemporalAdjusters.lastDayOfMonth());// len
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy年M月");
+			System.out.print("      " + firstDateOfMonth.format(formatter) + "\n");
+			System.out.println("---------------------");
+			System.out.println("日  一  二  三 四  五  六");
+			System.out.println("=====================");
+			int dayOfWeekStarIndex = firstDateOfMonth.getDayOfWeek().getValue();// 獲取月份的第一天是星期幾(要從index裡面get出value)
+			if (dayOfWeekStarIndex == 7) {
+				dayOfWeekStarIndex = 0;// 將星期天的位置換到index0
+			}
+			for (int i = 0; i < dayOfWeekStarIndex; i++) {
+				System.out.print("   ");// 根據星期幾換算出前面需要幾個空格
+			}
+			while (!firstDateOfMonth.isAfter(lastDateOfMoonth)) {// while迴圈會持續到firstDateOfMonth超過lastDateOfMoonth才會停下
+				System.out.printf("%2d ", firstDateOfMonth.getDayOfMonth());// 限制格式列印這個月
+				if (firstDateOfMonth.getDayOfWeek().getValue() == 6) {// 如果值是星期六就換行
+					System.out.println();
+				}
+				firstDateOfMonth = firstDateOfMonth.plusDays(1);// currentDay+1繼續下一次循環，直到while迴圈結束
+
+			}
+
 			System.out.println();
 		}
-		scanner.close();
+
 	}
 
 }
--- Java/practice/Cars.java
+++ Java/practice/Cars.java
@@ -16,28 +16,25 @@ import java.util.TreeMap;
 
 public class Cars {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String inputFilePath = "C:\\Users\\Admin\\Desktop\\Java評量_第6題cars.csv";
 		String outputFilePath = "C:\\Users\\Admin\\Desktop\\cars2.csv";
-
+		String headerLine = "";
 		List<Map<String, String>> carList = new ArrayList<>(); // 創建一個用來儲存汽車資料的 List，每個元素是一個 Map
-
 		// 讀取 CSV 文件
 		try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
-
-			String headerLine = br.readLine(); // 讀取表頭，不做任何處理
+			headerLine = br.readLine(); // 讀取表頭，不做任何處理//readLine是讀取第一行
 			String line;
 			while ((line = br.readLine()) != null) { // 逐行讀取CSV文件
 				String[] values = line.split(","); // 將每行的資料按逗號分割
-				if (values.length == 4) {
-					Map<String, String> carMap = new HashMap<>();
-					carMap.put("Manufacturer", values[0].trim());
-					carMap.put("TYPE", values[1].trim());
-					carMap.put("Min.PRICE", values[2].trim());
-					carMap.put("Price", values[3].trim());
-					carList.add(carMap); // 把這個 Map 放入 List 中
+//				if (values.length == 4) {// 確保一定有四筆才會放入，這個可以不加
+				Map<String, String> carMap = new HashMap<>();
+				carMap.put("Manufacturer", values[0].trim());
+				carMap.put("TYPE", values[1].trim());
+				carMap.put("Min.PRICE", values[2].trim());
+				carMap.put("Price", values[3].trim());
+				carList.add(carMap); // 把這個 Map 放入 List 中
 //	                    System.out.println(carList);
-				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -52,18 +49,23 @@ public class Cars {
 			}
 		});
 
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath))) {
-			bw.write("Manufacturer,TYPE,Min.PRICE,Price");
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilePath));) {
+
+			bw.write(headerLine);// 可以把headerLine
 			bw.newLine();
 			for (Map<String, String> car : carList) {
-				bw.write(car.get("Manufacturer") + "," + car.get("TYPE") + "," + car.get("Min.PRICE") + ","
-						+ car.get("Price"));
+				StringBuilder carInfo = new StringBuilder();
+				carInfo.append(car.get("Manufacturer")).append(',').append(car.get("TYPE")).append(',')
+						.append(car.get("Min.PRICE")).append(',').append(car.get("Price"));
+				String carInfo1 = carInfo.toString();
+				bw.write(carInfo1);// sb
 				bw.newLine();
 			}
 		} catch (Exception e) {
-
+			throw e;
 		}
-		Map<String, List<Map<String, String>>> groupedCars = new TreeMap<>();//原hash改tree
+
+		Map<String, List<Map<String, String>>> groupedCars = new TreeMap<>();// 原hash改tree
 
 		for (Map<String, String> car : carList) {
 			String manufacturer = car.get("Manufacturer");
@@ -72,11 +74,11 @@ public class Cars {
 		}
 
 		// 打印分组后的數據，計算小計和總計
-		BigDecimal totalMinPrice = BigDecimal.ZERO;
+		BigDecimal totalMinPrice = BigDecimal.ZERO;// 合計歸零
 		BigDecimal totalPrice = BigDecimal.ZERO;
 		System.out.printf("%-15s%-10s%10s%10s\n", "Manufacturer", "TYPE", "Min.PRICE", "Price");
 		for (String manufacturer : groupedCars.keySet()) {
-			BigDecimal manufacturerMinPriceTotal = BigDecimal.ZERO;
+			BigDecimal manufacturerMinPriceTotal = BigDecimal.ZERO;// 小計歸零
 			BigDecimal manufacturerPriceTotal = BigDecimal.ZERO;
 
 			for (Map<String, String> car : groupedCars.get(manufacturer)) {
