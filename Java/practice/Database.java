c995d1dcac73d72d7870ce10bcfc58a6ce0e6816
Joyce_00550348 <joyce41221@gmail.com>
2024-08-15 08:59:50 +0800

    【JAVA】第五次push

--- Java/practice/Database.java
+++ Java/practice/Database.java
@@ -1,4 +1,4 @@
-package com.cathaybk.javaedu.lesson3.practice;
+package com.cathaybk.practice.nt50348.b;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
@@ -11,23 +11,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Database {
 	private static final String SELECT_CARS_SQL = "select * from  STUDENT.CARS";
 
+	private static final String SELECT_CARS_SQL1 = "select * from  STUDENT.CARS WHERE MANUFACTURER=? AND TYPE=?";
+
 	private static final String INSERT_CARS_SQL = "insert into STUDENT.CARS (MANUFACTURER, TYPE, MIN_PRICE, PRICE) values (?, ?, ?, ?)";
+
+	private static final String UPDATE_STRING = "update STUDENT.CARS set MIN_PRICE =?,PRICE=? WHERE MANUFACTURER=? AND TYPE=?";
+
+	private static final String DELETE_STRING = "delete from  STUDENT.CARS  WHERE MANUFACTURER=? AND TYPE=?";
 
 	private static final String CONN_URL = "jdbc:oracle:thin:@//localhost:1521/XE";
 
+	private static final String USER_NAME = "student";
+
+	private static final String USER_PASSWORD = "student123456";
+
+	// SQL語法跟連線語法(user跟pw)都要放外面
+
 	public static void main(String[] args) {
 		ResultSet rs = null;
-		Connection conn = null;
-		
+
 		List<Map<String, String>> carList = new ArrayList<>();
-		try {
-			conn = DriverManager.getConnection(CONN_URL, "student", "student123456");
+		try (Connection conn = DriverManager.getConnection(CONN_URL, USER_NAME, USER_PASSWORD);
+				PreparedStatement pstmt = conn.prepareStatement(SELECT_CARS_SQL);) {
 			conn.setAutoCommit(false);
-			PreparedStatement pstmt = conn.prepareStatement(SELECT_CARS_SQL);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
 				Map<String, String> map = new HashMap<String, String>();
@@ -37,65 +48,91 @@ public class Database {
 				map.put("PRICE", rs.getString("PRICE"));
 
 				carList.add(map);
-				
-			}for (Map<String, String> map : carList) {
 				System.out.println(map);
 			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
 
+		} catch (SQLException e) {// SQLException
+			e.printStackTrace();
 		}
-		try (Scanner scanner = new Scanner(System.in)) {
+
+		try (Scanner scanner = new Scanner(System.in);) {
 			System.out.println("請選擇以下指令輸入:select、insert、update、delete");
 			String operation = scanner.next();
-			try {
-				conn = DriverManager.getConnection(CONN_URL, "student", "student123456");
+			try (Connection conn = DriverManager.getConnection(CONN_URL, USER_NAME, USER_PASSWORD);) {
 				switch (operation) {
 				case "select":
-					query(conn);
-					break;
+					try (Scanner scanner2 = new Scanner(System.in);) {
+						System.out.println("請輸入製造商:");
+						String manufacturer = scanner2.nextLine();
+						System.out.println("請輸入類型:");
+						String type = scanner2.nextLine();
+						System.out.println("請輸入底價:");
+						int minprice = scanner2.nextInt();
+						System.out.println("請輸入售價:");
+						int price = scanner2.nextInt();
+					query(manufacturer,type);
+					}break;
 				case "insert":
 					insert(conn);
 					break;
 				case "update":
-					update(conn);
+					boolean success = false;
+					while (!success) {
+						try (Scanner scanner1 = new Scanner(System.in);) {
+							System.out.println("請輸入製造商:");
+							String manufacturer = scanner1.nextLine();
+							System.out.println("請輸入類型:");
+							String type = scanner1.nextLine();
+							System.out.println("請輸入新底價:");
+							int minprice = scanner1.nextInt();
+							System.out.println("請輸入新售價:");
+							int price = scanner1.nextInt();
+							
+
+							Map<String, String> updateMap = new TreeMap<>();
+							updateMap.put("MANUFACTURER", manufacturer);
+							updateMap.put("TYPE", type);
+							updateMap.put("MIN_PRICE", Integer.toString(minprice));
+							updateMap.put("PRICE", Integer.toString(price));// int轉String
+							success=update(updateMap);
+						}
+					}
 					break;
+
 				case "delete":
 					delete(conn);
 					break;
 				default:
 					System.out.println("重新選擇");
 				}
-			} catch (Exception e) {
-				
+			} catch (SQLException e) { // 有catch要做事
+				e.printStackTrace();
 			}
 		}
 	}
 
-	private static void query(Connection conn) throws SQLException {
-		try (PreparedStatement pstmt = conn.prepareStatement(SELECT_CARS_SQL); ResultSet rs = pstmt.executeQuery()) {
+	private static void query(String manufacturer, String type)throws SQLException { 
+		
+		try (Connection conn = DriverManager.getConnection(CONN_URL, USER_NAME, USER_PASSWORD);
+				PreparedStatement pstmt = conn.prepareStatement(SELECT_CARS_SQL1);) {
+			ResultSet rs = null;
+		
 			while (rs.next()) {
+	
 				System.out.println("製造商: " + rs.getString("MANUFACTURER") + ", 型號: " + rs.getString("TYPE") + ", 底價: "
 						+ rs.getInt("MIN_PRICE") + ", 售價: " + rs.getInt("PRICE"));
-			}rs.close();
+			}
+			rs.close();
 		} catch (Exception e) {
-			
+
 		} finally {
 
 		}
 	}
 
+
 	private static void insert(Connection conn) throws SQLException {
-		try (Scanner scanner = new Scanner(System.in)) {
+		try (Scanner scanner = new Scanner(System.in);) {
 			System.out.println("請輸入製造商:");
 			String manufacturer = scanner.nextLine();
 			System.out.println("請輸入類型:");
@@ -128,49 +165,47 @@ public class Database {
 		}
 	}
 
-	private static void update(Connection conn) throws SQLException {
-		String updateString = "UPDATE STUDENT.CARS SET MIN_PRICE =?,PRICE=? WHERE MANUFACTURER=? AND TYPE=?";
-		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println("請輸入製造商:");
-			String manufacturer = scanner.nextLine();
-			System.out.println("請輸入類型:");
-			String type = scanner.nextLine();
-			System.out.println("請輸入新底價:");
-			BigDecimal minprice = scanner.nextBigDecimal();
-			System.out.println("請輸入新售價:");
-			BigDecimal price = scanner.nextBigDecimal();
-			try (PreparedStatement pstmt = conn.prepareStatement(updateString)) {
+	private static boolean update(Map<String, String> updateMap) throws SQLException {
+		
+			try (Connection conn = DriverManager.getConnection(CONN_URL, USER_NAME, USER_PASSWORD);
+					PreparedStatement pstmt = conn.prepareStatement(UPDATE_STRING);) {
 				conn.setAutoCommit(false);
-				pstmt.setBigDecimal(1, minprice);
-				pstmt.setBigDecimal(2, price);
-				pstmt.setString(3, manufacturer);
-				pstmt.setString(4, type);
+				pstmt.setInt(1, Integer.parseInt(updateMap.get("MIN_PRICE")));// String轉int
+				pstmt.setInt(2, Integer.parseInt(updateMap.get("PRICE")));
+				pstmt.setString(3, updateMap.get("MANUFACTURER").toString());
+				pstmt.setString(4, updateMap.get("TYPE").toString());
 				pstmt.executeUpdate();
 				conn.commit();
-				System.out.println("更新成功");
-			} catch (Exception e) {
-				System.out.println("更新失敗，原因：" + e.getMessage());
-				try {
-					conn.rollback();
-				} catch (SQLException sqle) {
-					sqle.printStackTrace();
-					System.out.println("rollback失敗，原因：" + sqle.getMessage());
+				if (pstmt.executeUpdate() == 0) {
+					return false;
+				} else {
+					
+					System.out.println("更新成功，已更新:" + pstmt.executeUpdate() + "筆");
+					
+					return true;
 				}
+			} catch (SQLException sqle) {
+				System.out.println("更新失敗，原因：" + sqle.getMessage());
 			}
-
-		} catch (Exception scan) {
-			scan.printStackTrace();
+			try (Connection conn = DriverManager.getConnection(CONN_URL, USER_NAME, USER_PASSWORD);) {
+				conn.setAutoCommit(false);
+				conn.rollback();
+			} catch (SQLException sqle) {
+				sqle.printStackTrace();
+				System.out.println("rollback失敗，原因：" + sqle.getMessage());
+			}
+			return false;
 		}
-	}
+	
 
 	private static void delete(Connection conn) {
-		String deleteString = "delete from  STUDENT.CARS  WHERE MANUFACTURER=? AND TYPE=?";
-		try (Scanner scanner = new Scanner(System.in)) {
+
+		try (Scanner scanner = new Scanner(System.in);) {
 			System.out.println("請輸入製造商:");
 			String manufacturer = scanner.nextLine();
 			System.out.println("請輸入類型:");
 			String type = scanner.nextLine();
-			try (PreparedStatement pstmt = conn.prepareStatement(deleteString)) {
+			try (PreparedStatement pstmt = conn.prepareStatement(DELETE_STRING);) {
 				conn.setAutoCommit(false);
 				pstmt.setString(1, manufacturer);
 				pstmt.setString(2, type);
